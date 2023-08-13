50fbfb9b6d05b2455feb584723d26cddf15fb41a
nurullah namal <nurullahnamal12@hotmail.com>
2023-08-13 12:34:52 +0300

    6.commint

--- RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -1,20 +1,34 @@
-﻿using Dapper;
-using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
 using RealEstate_Dapper_Api.Models.DapperContext;
+using Dapper;
 
 namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
 {
     public class CategoryRepository : ICategoryRepository
     {
         private readonly Context _context;
-        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        public CategoryRepository(Context context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public async Task<List<ResultCategoryDto>> GetAllCategoryAsyc()
+        public async void CreateCategory(CreateCategoryDto categoryDto)
         {
-            string query = "select * from Category";
+            string query = "insert into Category (CategoryName,CategoryStatus) values (@categoryName,@categoryStatus)";
+            var parameters = new DynamicParameters();
+            parameters.Add("categoryName", categoryDto.CategoryName);
+            parameters.Add("categoryStatus", true);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+       
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * From Category";
             using (var connection = _context.CreateConnection())
             {
                 var values = await connection.QueryAsync<ResultCategoryDto>(query);

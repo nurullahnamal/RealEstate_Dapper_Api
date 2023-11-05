f468778f7be21d16618857bd42cd2d227ccbf1d4
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-11-05 13:24:58 +0300

    28.update

---
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepositories/EmployeeREpository.cs
@@ -0,0 +1,33 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.EmployeeRepositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void CreateEmployee(CreateEmployeeDto createEmployeeDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultEmployeeDto>> GetAllEmployeeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByCategoryDto> GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(UpdateEmployeeDto updateEmployee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RealEstate_Dapper_UI/Dtos/ProductDtos/CreateProductDto.cs
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/CreateProductDto.cs
@@ -7,7 +7,7 @@
         public decimal price { get; set; }
         public string city { get; set; }
         public string district { get; set; }
-        public string categoryName { get; set; }
+        public string categoryId { get; set; }
         public string coverimage { get; set; }
         public string type { get; set; }
         public string address { get; set; }

a542a0d90adf1c44350f6705b74254f66a531026
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-11-08 09:14:02 +0300

    38.Create -update -delete-get-Service

--- RealEstate_Dapper_Api/Controllers/ServicesController.cs
+++ RealEstate_Dapper_Api/Controllers/ServicesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 
 namespace RealEstate_Dapper_Api.Controllers
 {
@@ -12,13 +14,38 @@ namespace RealEstate_Dapper_Api.Controllers
 
         public ServicesController(IServiceRepository serviceRepository)
         {
-            _serviceRepository= serviceRepository;
+            _serviceRepository = serviceRepository;
         }
 
+
         [HttpGet]
-        public async Task< IActionResult >GetServiceList()
+        public async Task<IActionResult> GetServiceList()
         {
-            var value = await _serviceRepository.GetAllServiceAsync();
+            var values = await _serviceRepository.GetAllServiceAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceDto  createServiceDto)
+        {
+            _serviceRepository.CreateService(createServiceDto);
+            return Ok("Hakkımızda Kısmı Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            _serviceRepository.DeleteService(id);
+            return Ok("Hakkımızda Kısmı Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            _serviceRepository.UpdateService(updateServiceDto);
+            return Ok("Hakkımızda Kısmı Başarıyla Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetService(int id)
+        {
+            var value = await _serviceRepository.GetService(id);
             return Ok(value);
         }
     }

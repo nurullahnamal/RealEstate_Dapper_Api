31da291b112dbabf5a434338be8361c89dda186c
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-06-05 21:00:14 +0300

    ToDoList

---
+++ RealEstate_Dapper_Api/Repositories/ToDoListRepositories/IToDoListRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate_Dapper_Api.Dtos.ToDoListDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.ToDoListRepositories;
+
+public interface IToDoListRepository
+{
+    Task<List<ResultToDoListDto>> GetAllToDoListAsync();
+    void CreateToDoList(CreateToDoListDto ToDoListDto);
+    void DeleteToDoList(int id);
+    void UpdateToDoList(UpdateToDoListDto ToDoListDto);
+    Task<GetByIDToDoListDto> GetToDoList(int id);
+}

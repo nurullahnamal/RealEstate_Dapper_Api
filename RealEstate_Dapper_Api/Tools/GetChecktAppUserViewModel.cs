3d0e01182b04c855f0b856f0e8baad90e64e92fe
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2024-06-08 21:34:18 +0300

    JWT

---
+++ RealEstate_Dapper_Api/Tools/GetChecktAppUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class GetChecktAppUserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role{ get; set; }
+        public bool IsExist{ get; set; }
+
+
+
+    }
+}
---
+++ RealEstate_Dapper_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "RealEstate..0123456789Asp.NetCore";
+        public const int Expire = 5;
+
+    }
+}
